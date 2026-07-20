d1341c3ca9bf6ef11fbf52cc2f8f777a9c64fb73
Lokesh <lokeshdepani982@gmail.com>
2026-07-20 10:45:53 +0530

    subset pattern

---
+++ BitManipulation/1863_Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(int mask=0;mask<(1<<n);mask++){
+            int subXOR=0;
+            for(int i=0;i<n;i++){
+                if(((mask>>i)&1))subXOR^=nums[i];
+            }
+            ans+=subXOR;
+        }
+      return ans;
+    }
+};
